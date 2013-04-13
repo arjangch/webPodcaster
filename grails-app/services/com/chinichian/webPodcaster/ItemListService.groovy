49bc1c7720fcb89c4870391e7296ccc9b605a960
Arjang Ch <arjangch@yahoo.com>
2013-04-13 16:50:39 -0400

    update

--- grails-app/services/com/chinichian/webPodcaster/ItemListService.groovy
+++ grails-app/services/com/chinichian/webPodcaster/ItemListService.groovy
@@ -2,14 +2,19 @@ package com.chinichian.webPodcaster
 import com.chinichian.webPodcaster.ItemVideo;
 
 class ItemListService { 
+
     def retriveItems(def items) {
         def itemList = []  
         for (item in items){
             ItemVideo itemVideo = new ItemVideo();
-            itemVideo.date = item.pubDate.text()
-            if(item.content.@url.text()) itemVideo.videoUrl = item.content.@url.text()
+            itemVideo.title = item.title.text()
+            
+            itemVideo.date = new Date(item.pubDate.text()).getDateString() 
+             
+            if (item.enclosure.@url.text()) itemVideo.videoUrl = item.enclosure.@url.text() 
+            else if(item.content.@url.text()) itemVideo.videoUrl = item.content.@url.text()
             else if(item.link.text()) itemVideo.videoUrl = item.link.text()
-            else if (item.enclosure.@url.text()) itemVideo.videoUrl = item.enclosure.@url.text()
+            
             itemList.add(itemVideo)
           }
           
