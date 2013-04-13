b3c46595cd07e409d1af3329d696f96f7dd39e8b
Arjang Ch <arjangch@yahoo.com>
2013-04-13 13:59:53 -0400

    add new sites and changed view.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,7 +5,20 @@ class BootStrap {
     def init = { servletContext ->
         new Podcasts(name: 'cnet', url: 'http://feeds.feedburner.com/allcnetvideopodcasts?format=xml').save(failOnError: true)  
         new Podcasts(name: 'nova', url: 'http://feeds.pbs.org/pbs/wgbh/nova-video').save(failOnError: true) 
-        new Podcasts(name: 'globalnews', url: 'http://globalnews.ca/toronto/feed/').save(failOnError: true) 
+        new Podcasts(name: 'Global National', url: 'http://feeds.feedburner.com/GlobalNationalVideoPodcast?format=xml').save(failOnError: true) 
+        new Podcasts(name: 'TED', url: 'http://feeds.feedburner.com/TedtalksHD?format=xml').save(failOnError: true) 
+        new Podcasts(name: 'The National with Rex Murphy', url: 'http://www.cbc.ca/mediafeeds/rss/cbc/rexmurphy-video-podcast.xml').save(failOnError: true) 
+        new Podcasts(name: 'Gadget Lab', url: 'http://downloads.wired.com/podcasts/xml/gadgetlab.xml').save(failOnError: true) 
+        new Podcasts(name: 'Nasa Cast', url: 'http://downloads.wired.com/podcasts/xml/gadgetlab.xml').save(failOnError: true)
+        new Podcasts(name: 'Hubble Cast', url: 'http://www.spacetelescope.org/rss/vodcastfullhd.xml').save(failOnError: true)
+        new Podcasts(name: 'Hidden Universe', url: 'http://www.spitzer.caltech.edu/features/hd/hdfeed.xml').save(failOnError: true)
+        new Podcasts(name: 'Dilbert', url: 'http://ringtales.com/dilbert.xml').save(failOnError: true)
+        new Podcasts(name: 'Fashion', url: 'http://www.style.com/services/rss/feeds/fashionshow_podcast.xml').save(failOnError: true)
+        new Podcasts(name: 'PBS - Nature', url: 'http://www.pbs.org/wnet/nature/rss/podcast.xml').save(failOnError: true)
+        new Podcasts(name: '', url: '').save(failOnError: true)
+        new Podcasts(name: '', url: '').save(failOnError: true)
+        new Podcasts(name: '', url: '').save(failOnError: true)
+        
     }
     def destroy = {
     }
--- grails-app/views/podcastsList/itemList.gsp
+++ grails-app/views/podcastsList/itemList.gsp
@@ -3,9 +3,10 @@
     <g:each var="item" in="${items}" status="i">
         <g:if test="${i < 30}">
             <p>
-                ${item.pubDate.text()} - <a href="${item.link.text()}">${item.title.text()}</a></br>
+                ${new Date(item.pubDate.text())} -- <a href="${item.content.@url.text()}">${item.content.@url.text()}</a> - <a href="${item.link.text()}">${item.link.text()}</a> - <a href="${item.enclosure.@url.text()}">${item.enclosure.@url.text()}</a>
             </p>
         </g:if>
+        
     </g:each>
 </body>
 </html>
